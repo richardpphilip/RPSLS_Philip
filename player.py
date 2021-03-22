4e14abf9c9b83ee15b0b59ff533391e14fe0da7f
richardpphilip <80114998+richardpphilip@users.noreply.github.com>
2021-03-22 14:11:18 -0700

    got the game to work.  still need to polish and make it more user friendly.  I also need to add catch's to change if you don't type in the correct syntax

--- player.py
+++ player.py
@@ -3,25 +3,12 @@ from user import User
 
 class Player(User):
 
-
-
     def __init__(self):
-        self.move = input("what move would you like to play?")
+        self.move = ''
         super().__init__()
 
     user_choice_list = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
-    def user_move(self):
-        if self.move == 'rock':
-            pass
-        if self.move == 'paper':
-            pass
-        if self.move == 'scissors':
-            pass
-        if self.move == 'lizard':
-            pass
-        if self.move == 'spock':
-            pass
 
 
 
