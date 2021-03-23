989f58ce9b1c3064485c4a480431c6b30017b8a0
richardpphilip <80114998+richardpphilip@users.noreply.github.com>
2021-03-23 09:53:34 -0700

    made a two player version and changed the inputs from hard code to all variable to make the code more reusable.  still need to debug

--- player.py
+++ player.py
@@ -4,11 +4,8 @@ from user import User
 class Player(User):
 
     def __init__(self):
-        self.move = ''
         super().__init__()
+        self.name = input('Please enter your name.')
+        self.move = ''
 
     user_choice_list = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-
-
-
-
